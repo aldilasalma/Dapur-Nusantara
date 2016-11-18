1a6f5a91abd1c802c5e8674ee5e0acaa555a3ec2
aldila <aldilasalmaa@gmail.com>
2016-11-17 16:57:44 -1200

    Adding Navigation Drawer

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl203132333/dapurnusantara/model/NavDrawerItem.java
@@ -0,0 +1,34 @@
+package id.sch.smktelkom_mlg.project.xirpl203132333.dapurnusantara.model;
+
+/**
+ * Created by aldila on 11/17/2016.
+ */
+public class NavDrawerItem {
+    private boolean showNotify;
+    private String title;
+
+    public NavDrawerItem() {
+    }
+
+    public NavDrawerItem(boolean showNotify, String title) {
+        this.showNotify = showNotify;
+        this.title = title;
+    }
+
+    public boolean isShowNotify() {
+        return showNotify;
+    }
+
+    public void setShowNotify(boolean showNotify) {
+        this.showNotify = showNotify;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
